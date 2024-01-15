6122921a1cd002edc327ba740405f4067182ee1a
Terry Park <terrypark@Terrys-MacBook-Air.local>
2024-01-15 15:32:11 -0800

    added shopping cart screen and a few other things

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import {Provider } from 'react-redux'
-import store from './store.js'
+import { Provider } from "react-redux";
+import store from "./store.js";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
@@ -15,12 +15,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
@@ -29,8 +31,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router} />
-
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
