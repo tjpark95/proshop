a296544a2bb34512c554a9bd43b6bcc1325cb352
Terry Park <terrypark@Terrys-MacBook-Air.local>
2024-01-24 21:19:35 -0800

    added checkout steps component, created dummy routes for orders

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+import asyncHandler from "../middleware/asyncHandler.js";
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+} from "../controllers/orderController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route("/mine").get(protect, getMyOrders);
+router.route("/:id").get(protect, admin, getOrderById);
+router.route("/:id/pay").put(protect, admin, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
+
+export default router;
