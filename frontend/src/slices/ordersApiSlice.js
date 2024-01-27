132341e648987ed91e572c5428157ef6285d673d
Terry Park <terrypark@Terrys-MacBook-Air.local>
2024-01-27 15:39:52 -0800

    order screen created

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -10,8 +10,14 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
         method: 'POST',
         body: {...order}
       })
+    }),
+    getOrderDetails: builder.query({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}`
+      }),
+      keepUnusedDataFor: 5,
     })
   })
 })
 
-export const { useCreateOrderMutation } = ordersApiSlice;
+export const { useCreateOrderMutation, useGetOrderDetailsQuery } = ordersApiSlice;
