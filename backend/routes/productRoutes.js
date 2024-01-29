5addbedcf2debf42c27af557be31f1eb8a9691b9
Terry Park <terrypark@Terrys-MacBook-Air.local>
2024-01-28 21:31:33 -0800

    created backend routes for creating/editting products and product edit screen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,9 +5,12 @@ import Product from "../models/productModel.js";
 import {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
 
 export default router;
